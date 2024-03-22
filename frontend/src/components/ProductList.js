bb5995a8cbaf4b1e82e65f92e2e134f7a9d4cde4
Anchit20 <anchitgedekar@gmail.com>
2024-03-22 08:23:39 +0530

    search-functionality

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -29,9 +29,34 @@ const ProductList = () => {
     setProducts(result);
   };
 
+  const handleSearch = async (e) => {
+    // console.log(e.target.value);
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+    let result = await fetch(`http://localhost:5000/search/${key}`);
+    result = await result.json();
+    if (result) {
+      setProducts(result);
+    }
+  };
+
   return (
     <div className="product-list">
       <h2>Product list</h2>
+      <input
+        className="searchBar"
+        type="text"
+        placeholder="search product"
+        onChange={handleSearch}
+      />
       <ul>
         <li>S. no.</li>
         <li>name</li>
@@ -39,18 +64,22 @@ const ProductList = () => {
         <li>category</li>
         <li>operation</li>
       </ul>
-      {products.map((item, index) => (
-        <ul key={item._id}>
-          <li>{index + 1}</li>
-          <li>{item.name}</li>
-          <li>{item.price}</li>
-          <li>{item.category}</li>
-          <li>
-            <button onClick={() => handleDelete(item._id)}>delete</button>
-            <button onClick={() => handleUpdate(item._id)}>update</button>
-          </li>
-        </ul>
-      ))}
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            <li>
+              <button onClick={() => handleDelete(item._id)}>delete</button>
+              <button onClick={() => handleUpdate(item._id)}>update</button>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>no products found</h1>
+      )}
     </div>
   );
 };
